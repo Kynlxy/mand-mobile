15e1fc15512b189f9880fa9d7f2510df388db1b5
p2p-wx <690044379@qq.com>
2019-03-19 14:25:46 +0800

    Update demo0.vue

--- components/tab-bar/demo/cases/demo0.vue
+++ components/tab-bar/demo/cases/demo0.vue
@@ -7,7 +7,8 @@
   </div>
 </template>
 
-<script>import {TabBar} from 'mand-mobile'
+<script>
+import {TabBar} from 'mand-mobile'
 
 export default {
   name: 'tab-bar-demo',
@@ -16,9 +17,10 @@ export default {
   },
   data() {
     return {
-      current: 1,
+      current: 3,
       items: [{name: 1, label: '标签1'}, {name: 2, label: '标签2'}],
     }
   },
 }
-</script>
+
+</script>
